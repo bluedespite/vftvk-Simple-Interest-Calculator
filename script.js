4db55b00265cab42eb1330baf369b5e3050fc3b5
Miguel Aguirre <miguelaguirre@gmail.com>
2021-05-17 01:16:05 +0000

    realizado

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal=document.getElementById("principal").value;
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+    var interest=principal*years*rate/100;
+    var year=new Date().getFullYear()+parseInt(years);
+    var s=document.getElementById("result");
+    s.innerHTML="<br> If you Deposit <mark>"+principal+ "</mark>,<br> at an interest rate of <mark>"+rate+"%</mark>, <br> You receive an amount of <mark>"+interest+"</mark>, <br> in the year <mark>"+year+"</mark>";
+} 
+
+function updateRate()
+{
+    var rateval=document.getElementById("rate").value;
+    var m=document.getElementById("rate_val");
+    m.innerText=rateval+"%";
+}
+function Validation()
+{
+    var prin=document.getElementById("principal");
+    if (prin.value<=0)
+    {alert("Enter a positive numer");
+    prin.focus;
+    }
 }
         
